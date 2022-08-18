9c4d5f592851f3ea565b59c30dfa3507c6a44985
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2022-08-17 23:41:04 -0300

    feat: create functions encoder_helper and perform_feature_engineering

--- churn_library.py
+++ churn_library.py
@@ -5,12 +5,32 @@ Author: Rudi César Comitto Modena
 Date: August, 2022
 """
 
+from typing import NoReturn, Optional, List
 import os
+
+import numpy as np
 import pandas as pd
+
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
-from typing import NoReturn
+import seaborn as sns
+sns.set()
+
+import shap
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import plot_roc_curve, classification_report
+
 import constants as const
+
+
 
 os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
@@ -34,23 +54,34 @@ def import_data(pth: str) -> pd.DataFrame:
     return pd_df
 
 
-def create_response_variable(pd_df, response=const.RESPONSE_VARIABLE, base_variable=const.RESPONSE_BASE_VARIABLE, custumer_churn_value=const.COSTUMER_CHURN_VALUE) -> pd.DataFrame:
+def create_response_variable(pd_df: pd.DataFrame,
+                             base_variable: Optional[str] = const.RESPONSE_BASE_VARIABLE,
+                             custumer_churn_value: Optional[str] = const.COSTUMER_CHURN_VALUE) -> pd.DataFrame:
     '''
     create the response variable according to a criterion
     input:
             pd_df: pandas dataframe
-            response:  string of response name
             base_variable:  string of base variable
             custumer_churn_value:  string with the value of the costumer that will churn
 
     output:
             pd_df: pandas dataframe with the new column
     '''
-    pd_df[response] = pd_df[base_variable].apply(lambda val: 1 if val == custumer_churn_value else 0)
+    
+    pd_df[const.RESPONSE_VARIABLE] = pd_df[base_variable].apply(
+        lambda val: 1 if val == custumer_churn_value else 0)
 
     return pd_df
 
-def plot_histogram(pd_df, variable, filename, width=const.EDA_FIGURE_WIDTH, height=const.EDA_FIGURA_HEIGHT, stat='count', kde=False) -> NoReturn:
+
+def plot_histogram(
+        pd_df: pd.DataFrame,
+        variable: str,
+        filename: str,
+        width: Optional[int]=const.EDA_FIGURE_WIDTH,
+        height: Optional[int]=const.EDA_FIGURE_HEIGHT,
+        stat: Optional[str]='count',
+        kde: Optional[bool] = False) -> NoReturn:
     '''
     create a histogram and save it as a png file
     input:
@@ -65,12 +96,16 @@ def plot_histogram(pd_df, variable, filename, width=const.EDA_FIGURE_WIDTH, heig
     output:
             None
     '''
-    plt.figure(figsize=(width,height)) 
+    plt.figure(figsize=(width, height))
     sns.histplot(data=pd_df, x=variable, stat=stat, kde=kde)
     plt.savefig(filename, bbox_inches='tight')
 
 
-def plot_heatmap(pd_df, filename, widht=const.EDA_FIGURE_WIDTH, height=const.EDA_FIGURA_HEIGHT) -> NoReturn:
+def plot_heatmap(
+    pd_df: pd.DataFrame,
+    filename: str,
+    widht:Optional[int] = const.EDA_FIGURE_WIDTH,
+    height:Optional[int] = const.EDA_FIGURE_HEIGHT) -> NoReturn:
     '''
     create a histogram and save it as a png file
     input:
@@ -82,12 +117,17 @@ def plot_heatmap(pd_df, filename, widht=const.EDA_FIGURE_WIDTH, height=const.EDA
     output:
             None
     '''
-    plt.figure(figsize=(widht,height)) 
-    sns.heatmap(pd_df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+    plt.figure(figsize=(widht, height))
+    sns.heatmap(pd_df.corr(), annot=False, cmap='Dark2_r', linewidths=2)
     plt.savefig(filename, bbox_inches='tight')
 
 
-def plot_categories(pd_df, variable, filename, widht=const.EDA_FIGURE_WIDTH, height=const.EDA_FIGURA_HEIGHT) -> NoReturn:
+def plot_categories(
+        pd_df: pd.DataFrame,
+        variable: str,
+        filename: str,
+        widht: Optional[int] = const.EDA_FIGURE_WIDTH,
+        height: Optional[int] = const.EDA_FIGURE_HEIGHT) -> NoReturn:
     '''
     create a histogram and save it as a png file
     input:
@@ -100,8 +140,11 @@ def plot_categories(pd_df, variable, filename, widht=const.EDA_FIGURE_WIDTH, hei
     output:
             None
     '''
-    plt.figure(figsize=(widht,height)) 
-    pd_df[variable].value_counts('normalize').plot(kind='bar').get_figure().savefig(filename, bbox_inches='tight')
+    plt.figure(figsize=(widht, height))
+    pd_df[variable].value_counts('normalize').plot(
+        kind='bar').get_figure().savefig(
+        filename, bbox_inches='tight')
+
 
 def perform_eda(pd_df: pd.DataFrame) -> NoReturn:
     '''
@@ -117,38 +160,59 @@ def perform_eda(pd_df: pd.DataFrame) -> NoReturn:
     pd_df = create_response_variable(pd_df=pd_df)
 
     # Create the plots
-    plot_histogram(pd_df=pd_df, variable=const.RESPONSE_VARIABLE, filename=const.EDA_CHURN_DISTRIB_FILENAME)
-    plot_histogram(pd_df=pd_df, variable=const.EDA_CUSTOMER_AGE_VARIABLE, filename=const.EDA_CUSTOMER_AGE_DISTRIB_FILENAME)
-    plot_histogram(pd_df=pd_df, variable=const.EDA_TOTAL_TRANSACTION_VARIABLE, filename=const.EDA_TOTAL_TRANSACTION_FILENAME, stat='density', kde=True)
+    plot_histogram(
+        pd_df=pd_df,
+        variable=const.RESPONSE_VARIABLE,
+        filename=const.EDA_CHURN_DISTRIB_FILENAME)
+    plot_histogram(
+        pd_df=pd_df,
+        variable=const.EDA_CUSTOMER_AGE_VARIABLE,
+        filename=const.EDA_CUSTOMER_AGE_DISTRIB_FILENAME)
+    plot_histogram(
+        pd_df=pd_df,
+        variable=const.EDA_TOTAL_TRANSACTION_VARIABLE,
+        filename=const.EDA_TOTAL_TRANSACTION_FILENAME,
+        stat='density',
+        kde=True)
     plot_heatmap(pd_df=pd_df, filename=const.EDA_HEATMAP_FILENAME)
-    plot_categories(pd_df=pd_df, variable=const.EDA_MARITAL_STATUS_VARIABLE, filename=const.EDA_MARITAL_STATUS_DISTR_FILENAME)
-    
+    plot_categories(
+        pd_df=pd_df,
+        variable=const.EDA_MARITAL_STATUS_VARIABLE,
+        filename=const.EDA_MARITAL_STATUS_DISTR_FILENAME)
 
 
-def encoder_helper(pd_df: pd.DataFrame, category_lst: list, response=None):
+def encoder_helper(pd_df: pd.DataFrame, category_lst: List[str], response: str = "") -> pd.DataFrame:
     '''
     helper function to turn each categorical column into a new column with
-propotion of churn for each category - associated with cell 15 from the
-notebook
+one-hot encoding transformation
 
     input:
             pd_df: pandas dataframe
             category_lst: list of columns that contain categorical features
-response: string of response name [optional argument that could be used
-for naming variables or index y column]
+            response: string of response name [optional argument that could be used
+            for naming variables or index y column]
 
     output:
             pd_df: pandas dataframe with new columns for
     '''
-    pass
+    # Rename response variable
+    if response:
+        pd_df.rename(columns={const.RESPONSE_VARIABLE: response}, inplace=True)
+
+    enc = make_column_transformer((
+        OneHotEncoder(handle_unknown='ignore'), category_lst),
+        remainder='passthrough')
+
+    np_enc = enc.fit_transform(pd_df)
+    return pd.DataFrame(np_enc, columns=enc.get_feature_names())
 
 
-def perform_feature_engineering(pd_df: pd.DataFrame, response):
+def perform_feature_engineering(pd_df: pd.DataFrame, response: str = "") -> List[np.ndarray]:
     '''
     input:
-              pd_df: pandas dataframe
-response: string of response name [optional argument that could be used
-for naming variables or index y column]
+                pd_df: pandas dataframe
+                response: string of response name [optional argument that could be used
+                for naming variables or index y column]
 
     output:
               X_train: X training data
@@ -156,6 +220,22 @@ for naming variables or index y column]
               y_train: y training data
               y_test: y testing data
     '''
+
+    # One-hot encoding categorical variables
+    pd_df = encoder_helper(pd_df, const.CAT_COLUMNS, response)
+
+    # Remove columns
+    pd_df.drop(columns=const.REMOVE_COLUMNS, inplace=True)
+
+    if response:
+        y = pd_df.pop(response)
+    else:
+        y = pd_df.pop(const.RESPONSE_VARIABLE)
+
+    X = pd_df
+
+    # # Train test split
+    return train_test_split(X, y, test_size=const.TEST_SIZE, random_state=const.RANDOM_STATE)
 
 
 def classification_report_image(y_train,
@@ -195,7 +275,7 @@ def feature_importance_plot(model, X_data, output_pth) -> NoReturn:
     pass
 
 
-def train_models(X_train, X_test, y_train, y_test) -> NoReturn:
+def train_models(X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray) -> NoReturn:
     '''
     train, store model results: images + scores, and store models
     input:
@@ -206,11 +286,33 @@ def train_models(X_train, X_test, y_train, y_test) -> NoReturn:
     output:
               None
     '''
-    pass
+    print(X_train)
+    print(y_train)
+    # grid search
+    rfc = RandomForestClassifier(random_state=const.RANDOM_STATE)
+
+    # Use a different solver if the default 'lbfgs' fails to converge
+    # Reference: https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
+    lrc = LogisticRegression(solver=const.LRC_SOLVER, max_iter=const.LRC_MAX_ITER)
+
+    # cv_rfc = GridSearchCV(estimator=rfc, param_grid=const.PARAM_GRID, cv=const.CROSS_VALID)
+    # cv_rfc.fit(X_train, y_train)
+
+    lrc.fit(X_train, y_train)
+
+    # save best model
+    joblib.dump(cv_rfc.best_estimator_, const.RFC_MODEL_FILENAME)
+    joblib.dump(lrc, const.LOGISTIC_MODEL_FILENAME)
+
+    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
+    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+
+    y_train_preds_lr = lrc.predict(X_train)
+    y_test_preds_lr = lrc.predict(X_test)
 
 
 if __name__ == "__main__":
     # Execute all the steps of the process
     pd_df = import_data(const.DATASET_PATH)
     perform_eda(pd_df)
-
+    train_models(*perform_feature_engineering(pd_df))
--- constants.py
+++ constants.py
@@ -16,44 +16,58 @@ RESPONSE_BASE_VARIABLE: Final[str] = "Attrition_Flag"
 COSTUMER_CHURN_VALUE: Final[str] = "Attrited Customer"
 
 EDA_FIGURE_WIDTH: Final[int] = 20
-EDA_FIGURA_HEIGHT: Final[int] = 10
+EDA_FIGURE_HEIGHT: Final[int] = 10
 
-EDA_CHURN_DISTRIB_FILENAME: Final[str] = "./images/eda/churn_distribution.png"
+EDA_FIGURE_FOLDER: Final[str] = "./images/eda/"
 
-EDA_HEATMAP_FILENAME: Final[str] = "./images/eda/heatmap.png"
+EDA_CHURN_DISTRIB_FILENAME: Final[str] = EDA_FIGURE_FOLDER + "churn_distribution.png"
+
+EDA_HEATMAP_FILENAME: Final[str] = EDA_FIGURE_FOLDER + "heatmap.png"
 
 EDA_CUSTOMER_AGE_VARIABLE: Final[str] = "Customer_Age"
-EDA_CUSTOMER_AGE_DISTRIB_FILENAME: Final[str] = "./images/eda/customer_age_distribution.png"
+EDA_CUSTOMER_AGE_DISTRIB_FILENAME: Final[str] = EDA_FIGURE_FOLDER + "customer_age_distribution.png"
 
 EDA_MARITAL_STATUS_VARIABLE: Final[str] = "Marital_Status"
-EDA_MARITAL_STATUS_DISTR_FILENAME: Final[str] = "./images/eda/marital_status_distribution.png"
+EDA_MARITAL_STATUS_DISTR_FILENAME: Final[str] = EDA_FIGURE_FOLDER + "marital_status_distribution.png"
 
 EDA_TOTAL_TRANSACTION_VARIABLE: Final[str] = "Total_Trans_Ct"
-EDA_TOTAL_TRANSACTION_FILENAME: Final[str] = "./images/eda/total_transaction_distribution.png"
+EDA_TOTAL_TRANSACTION_FILENAME: Final[str] = EDA_FIGURE_FOLDER + "total_transaction_distribution.png"
 
-# # Categorical Columns
-# cat_columns: Final[tuple] = (
-#     'Gender',
-#     'Education_Level',
-#     'Marital_Status',
-#     'Income_Category',
-#     'Card_Category'                
-# )
+RESULTS_FIGURE_FOLDER: Final[str] = "./images/results/"
 
-# # Categorical Columns
-# quant_columns: Final[tuple] = (
-#     'Customer_Age',
-#     'Dependent_count', 
-#     'Months_on_book',
-#     'Total_Relationship_Count', 
-#     'Months_Inactive_12_mon',
-#     'Contacts_Count_12_mon', 
-#     'Credit_Limit', 
-#     'Total_Revolving_Bal',
-#     'Avg_Open_To_Buy', 
-#     'Total_Amt_Chng_Q4_Q1', 
-#     'Total_Trans_Amt',
-#     'Total_Trans_Ct', 
-#     'Total_Ct_Chng_Q4_Q1', 
-#     'Avg_Utilization_Ratio'
-# )
+MODELS_FOLDER: Final[str] = "./models/"
+
+# Categorical Columns
+CAT_COLUMNS: Final[list] = [
+    'Gender',
+    'Education_Level',
+    'Marital_Status',
+    'Income_Category',
+    'Card_Category'                
+]
+
+# Remove Columns
+REMOVE_COLUMNS: Final[list] = [
+    'Unnamed: 0',
+    'CLIENTNUM',
+    'Attrition_Flag',
+]
+
+# Modeling
+TEST_SIZE: Final[float] = 0.3
+RANDOM_STATE: Final[int] = 42
+
+LRC_SOLVER: Final[str] = 'lbfgs'
+LRC_MAX_ITER: Final[int] = 3000
+
+PARAM_GRID: Final[dict] = { 
+    'n_estimators': [200, 500],
+    'max_features': ['auto', 'sqrt'],
+    'max_depth' : [4,5,100],
+    'criterion' :['gini', 'entropy']
+}
+
+CROSS_VALID: Final[int] = 5
+
+RFC_MODEL_FILENAME: Final[str] = './models/rfc_model.pkl'
+LOGISTIC_MODEL_FILENAME: Final[str] = './models/logistic_model.pkl'
